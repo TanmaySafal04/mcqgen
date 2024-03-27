21dfd0d32061e0ef78dcb9c1f671d0cc0f93f633
TanmaySafal04 <tanmaysafal.4@gmail.com>
2024-03-27 15:35:25 +0530

    updates files

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 # To install libraries in our local environment
+
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Tanmay safal',
+    author_email='tanmaysafal.4@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
